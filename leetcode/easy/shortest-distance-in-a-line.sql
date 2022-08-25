99d06ea0de0fc43b51502867c64de8d9dfc300b0
mangodm <dmhuh1003@gmail.com>
2022-08-25 20:18:38 +0900

    feat: add 6 solutions

---
+++ leetcode/easy/shortest-distance-in-a-line.sql
@@ -0,0 +1,41 @@
+-- Question 613: Shortest Distance in a Line
+
+-- Table: Point
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | x           | int  |
+-- +-------------+------+
+-- x is the primary key column for this table.
+-- Each row of this table indicates the position of a point on the X-axis.
+ 
+
+-- Write an SQL query to report the shortest distance between any two points from the Point table.
+
+-- The query result format is in the following example.
+
+-- Example 1:
+
+-- Input: 
+-- Point table:
+-- +----+
+-- | x  |
+-- +----+
+-- | -1 |
+-- | 0  |
+-- | 2  |
+-- +----+
+-- Output: 
+-- +----------+
+-- | shortest |
+-- +----------+
+-- | 1        |
+-- +----------+
+-- Explanation: The shortest distance is between points -1 and 0 which is |(-1) - 0| = 1.
+
+-- Solution
+SELECT MIN(ABS(p1.x - p2.x)) AS shortest
+FROM Point AS p1
+INNER JOIN Point AS p2
+WHERE p1.x != p2.x;
