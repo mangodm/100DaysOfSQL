6ac365f2bedc5755b779569d26c4e7f663060ab0
mangodm <dmhuh1003@gmail.com>
2022-09-12 00:19:04 +0900

    feat: add 1 solution

--- udacity/pdsnd/project.sql
+++ udacity/pdsnd/project.sql
@@ -38,3 +38,42 @@ SELECT
     END AS abc_rank
 FROM rentals_composition_ratio
 ORDER BY composition_ratio DESC;
+
+-- 02.
+-- Description
+-- Top 3 genres by country
+
+-- Solution
+WITH country_category_count_list AS (
+
+    SELECT
+        country.country,
+        category.name AS category_name,
+        COUNT(*) AS rental_count,
+        RANK() OVER (PARTITION BY country ORDER BY COUNT(*) DESC) AS rn
+    FROM rental
+    INNER JOIN inventory 
+        ON rental.inventory_id = inventory.inventory_id
+    INNER JOIN film
+        ON inventory.film_id = film.film_id
+    INNER JOIN film_category
+        ON film.film_id = film_category.film_id
+    INNER JOIN category
+        ON film_category.category_id = category.category_id
+    INNER JOIN customer
+        ON rental.customer_id = customer.customer_id
+    INNER JOIN address
+        ON customer.address_id = address.address_id
+    INNER JOIN city
+        ON address.city_id = city.city_id
+    INNER JOIN country
+        ON city.country_id = country.country_id
+    WHERE country.country IN ('India', 'China', 'United States')
+    GROUP BY category.name, country.country
+    
+)
+
+SELECT *
+FROM country_category_count_list
+WHERE rn <= 3
+ORDER BY country, rn;
