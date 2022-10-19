7ed4aa045bec104dbcd8ab2dd1cb4bba88552fd4
Dami Huh <39581869+mangodm@users.noreply.github.com>
2022-10-19 23:50:47 +0900

    feat: add 1 solution

---
+++ datalemur/medium/cumulative-purchases-by-product-type.sql
@@ -0,0 +1,9 @@
+-- Question : Cumulative Purchases by Product Type
+
+-- Solution
+SELECT
+    order_date,
+    product_type,
+    SUM(quantity) OVER (PARTITION BY product_type ORDER BY order_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cum_purchased
+FROM total_trans
+ORDER BY order_date;
